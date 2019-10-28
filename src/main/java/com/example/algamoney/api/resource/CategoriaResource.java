b44b84801e0a76276fe817de3c2f0e39ecd7e346
Rafael Rutsatz <rafael.rutsatz@velsis.com.br>
2019-10-28 15:25:37 -0300

    3.7. Desafio: Retornar 404 caso não exista a categoria

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -47,8 +47,9 @@ public class CategoriaResource {
 	}
 
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-		return this.categoriaRepository.findById(codigo).orElse(null);
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+		return this.categoriaRepository.findById(codigo).map(categoria -> ResponseEntity.ok(categoria))
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
