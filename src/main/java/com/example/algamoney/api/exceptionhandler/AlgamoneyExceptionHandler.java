f2fe9d6aa2d8a2a41385b016aeb0a6dd04a2b186
Rafael Rutsatz <rafael.rutsatz@velsis.com.br>
2019-10-28 17:11:22 -0300

    3.10. Validando valores inválidos com Bean Validation

--- src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.example.algamoney.api.exceptionhandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -40,9 +47,44 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
 		// Adiciona uma mensagem para o desenvolvedor, que está consumindo a API.
 		String mensagemDesenvolvedor = ex.getCause().toString();
+
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
 		// Peço para o Spring tratar a exceção. Só que eu consigo passar um body.
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers,
-				HttpStatus.BAD_REQUEST, request);
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	/**
+	 * Trata as mensagens quando o argumentos de um método ou request não são
+	 * válidos. Para ele validar, é necessário estar anotado com o @Valid.
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+		return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	/**
+	 * Trata para os casos de campos inválidos, pois a validação pode ter falhado em
+	 * vários campos.
+	 *
+	 * @return
+	 */
+	private List<Erro> criarListaDeErros(BindingResult bindingResult) {
+		List<Erro> erros = new ArrayList<>();
+
+		// Percorre todos os atributos que foram validados na entidade.
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+			// Usa o messageSource passando direto o fieldError, pois lá no arquivo de
+			// properties tratamos a mensagem para cada campo da entidade, e assim ele
+			// consegue recuperar as mensagens pelo código do @NotNull.
+			String mensagemUsuario = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			String mensagemDesenvolvedor = fieldError.toString();
+			erros.add(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		}
+		return erros;
 	}
 
 	/**
